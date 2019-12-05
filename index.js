918a42c4ce15659c6c9f267991c1d95067af4100
Alexandre Giroux <giroux.dion@gmail.com>
2019-12-05 11:23:36 -0500

    added test for mysqlDB

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const { Pool } = require('mysql');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 app
@@ -11,4 +16,27 @@ app
 
 app
   .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM buzzword');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+  .post('/ninjify', (req, res) =>{
+
+    if (req.param('x') != null) {
+      let queryBuzzword = querystring.stringify(req.query);
+      let parseBuzzword = querystring.parse(queryBuzzword, ',');
+      let buzzwords = Array.from(parseBuzzword); 
+    }
+    res.render('ninjify');
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
