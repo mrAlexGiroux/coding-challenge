7d337e164d7a3353ef04fd94912c0d62850c5d47
Alexandre Giroux <giroux.dion@gmail.com>
2019-12-05 10:17:14 -0500

    integrated views

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+app
+  .set('views',path.join(__dirname,'public'))
+  .set('view engine', 'ejs');
+
+app
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
---
+++ public/assets/js/ninjaNameGenerator.js
@@ -0,0 +1,10 @@
+
+
+module.exports =  {
+
+    ReturnNinjaName : async (buzzword) =>{
+
+        
+        return this.ReturnNinjaName;
+    }
+}
